fb52bd06a511a5828aa0a25ed9060274b09af60f
dlake82 <dlake0828@gmail.com>
2024-02-06 23:22:33 +0900

    chore: addon 초기화

--- keys/set.sh
+++ keys/set.sh
@@ -2,8 +2,3 @@
 sudo apt update
 sudo apt install python3 python3-pip git -y
 pip3 install -r requirements.txt
-
-# cp keys
-\cp -f id_rsa ~/.ssh/
-\cp -f id_rsa.pub ~/.ssh/
-\cp -f id_rsa.pub ~/.ssh/authorized_keys
