a3197e9f84568d61968ed0badea9824c9f64de38
dlake82 <dlake0828@gmail.com>
2024-02-14 02:11:07 +0900

    chore: setup 추가

--- set/cp.sh
+++
@@ -1,4 +0,0 @@
-# cp keys
-\cp -f id_rsa ~/.ssh/
-\cp -f id_rsa.pub ~/.ssh/
-\cp -f id_rsa.pub ~/.ssh/authorized_keys
--- set/set.sh
+++
@@ -1,4 +0,0 @@
-# update
-sudo apt update
-sudo apt install python3 python3-pip git -y
-pip3 install -r requirements.txt
